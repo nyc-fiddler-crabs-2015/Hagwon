0e1a39cd4a0149e2755620753dce9607e2a44f72
Rayanb <rayanbouts@gmail.com>
2015-03-27 14:30:50 -0400

    add welcome controller to display root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
- root => 'welcome#index'
+ root 'welcome#index'
  resources :users
  resources :sessions, only: [:new, :create, :destroy]
  resources :categories, only: [:index, :show]
