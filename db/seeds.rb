cf0b301c5ef993a54b6eb11a02847826688f51fc
Rayanb <rayanbouts@gmail.com>
2015-03-27 10:30:40 -0400

    Test angular with a backend, works well

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,7 @@
 # Track.last.courses && Course.last.tracks
 
 # get the order > CourseTrack.where(course_id: 1, track_id: 2).order
+
+10.times do
+  Track.create(user_id: 1, category_id: 2,  name: 'Cool Track')
+end
