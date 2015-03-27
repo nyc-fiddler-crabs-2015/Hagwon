27460408f104ebe561101be3d04979f15258914a
Rayanb <rayanbouts@gmail.com>
2015-03-27 17:17:19 -0400

    controller for tracks

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,4 +1,17 @@
 class TracksController < ApplicationController
+  def index
+    @tracks = Track.all
+  end
+
+  def show
+    @track = Track.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
 
 
 end
