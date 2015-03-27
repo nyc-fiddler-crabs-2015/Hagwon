770de83a484d5c86e075a27a8819326955873ed1
Rayanb <rayanbouts@gmail.com>
2015-03-27 16:13:47 -0400

    fix username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 	has_secure_password
-	validates_uniqueness_of :user_name
+	validates_uniqueness_of :username
 	validates_uniqueness_of :email
 	has_many :reviews
 	has_many :tracks
