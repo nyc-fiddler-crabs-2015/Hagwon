e483302fa0dc0eb00ab43f04010db259c3da4791
Rayanb <rayanbouts@gmail.com>
2015-03-27 14:21:50 -0400

    create restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
  resources :users
+ resources :sessions, only: [:new, :create, :destroy]
+ resources :categories, only: [:index, :show]
+ resources :courses do
+  resources :reviews
+end
+
+ resources :tracks
+
 end
