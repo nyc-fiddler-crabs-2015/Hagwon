8d4856fc4616b0a234000b739058114eea9f67ba
Rayanb <rayanbouts@gmail.com>
2015-03-27 17:16:38 -0400

    spec for tracks controller's index

--- spec/controllers/tracks_controller_spec.rb
+++ spec/controllers/tracks_controller_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 describe TracksController do
+  let!(:user){User.create(email: 'rayanbouts@gmail.com', username: 'rayanbouts', password: 'ilovejs')}
+  let!(:category){Category.create(name: 'Programming')}
+  let!(:track){Track.create(name: 'Learn python in no time', user: user, category: category)}
+
+  it 'has an index page' do
+    get :index
+    expect(response).to be_ok
+  end
+
 end
