184fe52ef376b8bafe5f78c9936838bd2fa463a6
gabalicious <gabalicious113@gmail.com>
2015-03-28 12:49:49 -0400

    Modified index controller to point to index view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,9 @@
 class WelcomeController < ApplicationController
 
   def index
-    if session[:user_id]
-      redirect_to user_path(session[:user_id])
-    else
-      render text: "I am the welcome page that presents our website"
-    end
+
+    redirect_to user_path(current_user.id) if current_user
+
   end
 
 end
