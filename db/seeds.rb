3f87718d0fcf9e39c369bcbc627763fe43928e3c
Rayanb <rayanbouts@gmail.com>
2015-03-28 13:49:58 -0400

    instructions on how to user join tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,11 @@
 
 # get the order > CourseTrack.where(course_id: 1, track_id: 2).order
 
+# User.first.tracks.create(user_id: 1, category_id: 1, name: 'yeee') will assign this track to an owner(User.first), and a user (means that the owner will be able to access it> adding a user_id will store the result in the join table, therefore making it accessible)
+
+#forking > UserTrack.create(user_id: session[:user_id], track_id: Track forked), after that User.find(session[:user_id]).tracks will include the newly forked track
+
+
 require 'open-uri'
 
 require 'json'
