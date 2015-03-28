8464286b3760c14205063f7702da2d5f7709bb12
Rayanb <rayanbouts@gmail.com>
2015-03-28 18:46:57 -0400

    angular view

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -1,4 +1,11 @@
 var Courses = angular.module('trackApp', [])
+
+Courses.config([
+  "$httpProvider", function($httpProvider) {
+      $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+  }
+]);
+
 Courses.controller('coursesCtrl', ['$scope', '$http', function($scope, $http){
   $scope.courses = [];
   $http.get(window.location.pathname+'/courses').then(function(response){
@@ -8,3 +15,5 @@ Courses.controller('coursesCtrl', ['$scope', '$http', function($scope, $http){
   })
 
 }])
+
+
