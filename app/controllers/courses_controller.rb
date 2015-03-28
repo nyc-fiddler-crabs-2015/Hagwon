288361068f66c870ad1f015f0671307537dc5a8b
bruce wayne <no1_ldc@hotmail.com>
2015-03-28 14:09:58 -0400

    Add course controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,14 +1,6 @@
 class CoursesController < ApplicationController
   def index
     @courses = Course.all
-    # if request.xhr?
-
-    #   courses = course.all.map{|cour| {id: cour.id, category: cour.category.name, url: cour.url, name: cour.user.name} }
-    #   render :json => {courses: courses}
-    # end
-
-    respond_to do |format|
-      format.html
-      format.json
-    end
+    render json: @courses
   end
+end
