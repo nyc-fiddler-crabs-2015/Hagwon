6ac977ca865876e7f55b106da13d783ec9c0c563
Rayanb <rayanbouts@gmail.com>
2015-03-28 17:10:00 -0400

    add route to courses json

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
  root 'welcome#index'
  post '/tracks/:track_id/fork', :to => 'tracks#fork'
  get '/tracks.json', :to => 'tracks#json'
+ get '/courses.json', :to => 'courses#json'
  resources :users
  resources :sessions, only: [:new, :create, :destroy]
  resources :categories, only: [:index, :show] do
   resources :tracks, only: [:new, :create]
  end
+
  resources :courses do
   resources :reviews
 end
