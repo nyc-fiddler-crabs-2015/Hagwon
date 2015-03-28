f9c02495d25535a5b3d57c8211e21805d6313a72
gabalicious <gabalicious113@gmail.com>
2015-03-28 18:06:18 -0400

    Added whitespace

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,7 @@ class ReviewsController < ActionController::Base
   def index
     @course = Course.find(params[:course_id])
     @reviews = @course.reviews
+    @review = Review.new
 
   end
 
@@ -11,12 +12,34 @@ class ReviewsController < ActionController::Base
   end
 
   def edit
-     
+
+  end
+
+  def create
+    
+    course = Course.find(params[:course_id])
+    review = course.reviews.new(review_params)
+    if review.save
+      redirect_to :back
+    else
+      redirect_to new_review_path
+    end
+    
   end
 
   def new
-      
+    @review = Review.new
   end
 
+  def destroy
+    Review.find(params[:id]).destroy
+    redirect_to :back
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:user_id, :difficulty, :rating)
+  end
   
 end
