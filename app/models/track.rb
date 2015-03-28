3d6eecf5c785cb6b9f32c16842752cd87a31c5df
Rayanb <rayanbouts@gmail.com>
2015-03-28 13:14:33 -0400

    add track_users relation, a track can be taken by many users

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,6 +1,7 @@
 class Track < ActiveRecord::Base
-  belongs_to :user
   belongs_to :category
   has_many :course_tracks
   has_many :courses, through: :course_tracks
+  has_many :user_tracks
+  has_many :users, through: :user_tracks
 end
