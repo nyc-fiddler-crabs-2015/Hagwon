df7c5cd29bbbc88e048891e4cac10bc996229862
bruce wayne <no1_ldc@hotmail.com>
2015-03-28 13:10:09 -0400

    Fixed test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,1 +1,11 @@
 require 'rails_helper'
+
+describe User do
+  it 'is valid with a username and email' do
+    user = User.new(
+      username: 'Justin',
+      email: 'test@example.com')
+    expect(user).to be_valid
+  end
+
+end
