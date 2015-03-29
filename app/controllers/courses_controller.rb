357f69441faea36022a66116bb0c5b4a1c1259f0
Rayanb <rayanbouts@gmail.com>
2015-03-29 11:01:38 -0400

    add eager loading to courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,7 +1,8 @@
 class CoursesController < ApplicationController
 
   def index
-    track = Track.find(params[:track_id])
+    track = Track.includes(:courses => :reviews).find(params[:track_id])
+    courses = track.courses.includes(:reviews)
     render json: track.courses.to_json(:include => :reviews)
   end
 
