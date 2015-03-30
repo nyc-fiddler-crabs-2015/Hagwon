2abfabf09ce04f6373875d70faafe7313337797c
Rayanb <rayanbouts@gmail.com>
2015-03-30 11:08:10 -0400

    add relation user and courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,8 @@ class Course < ActiveRecord::Base
   has_many :course_tracks
   has_many :reviews
   has_many :tracks, through: :course_tracks
+  has_many :user_courses
+  has_many :users, through: :user_courses
   belongs_to :category
 
   def rating
