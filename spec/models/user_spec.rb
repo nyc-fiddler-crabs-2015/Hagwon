b6dc0909a474e711eef66eaf98ea0a91251286a2
bruce wayne <no1_ldc@hotmail.com>
2015-03-30 11:55:12 -0400

    fix user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,8 @@ describe User do
   it 'is valid with a username and email' do
     user = User.new(
       username: 'Justin',
-      email: 'test@example.com', password: '1234', password_confirmation: '1234')
+      email: 'test@example.com',
+      password: '1234', password_confirmation: '1234')
     expect(user).to be_valid
   end
 
