9be5337643a716c0dd1d8cadfe1d9f24b7dd1c28
bruce wayne <no1_ldc@hotmail.com>
2015-03-30 11:55:40 -0400

    fix categoy_spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,8 +4,6 @@ describe Category do
    it 'is invalid without a category name' do
     category = Category.new(name: nil)
     categoty.valid?
-    expect(category.errors[:name]).to include("can't be bla")
-
-
+    expect(category.errors[:name]).to include("can't be blank")
   end
 end
