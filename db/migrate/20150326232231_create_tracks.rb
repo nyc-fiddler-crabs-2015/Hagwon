0c09da601f0ec28f31e10afe87696d96c6350920
Rayanb <rayanbouts@gmail.com>
2015-03-30 18:12:35 -0400

    add parent_id column

--- db/migrate/20150326232231_create_tracks.rb
+++ db/migrate/20150326232231_create_tracks.rb
@@ -4,6 +4,8 @@ class CreateTracks < ActiveRecord::Migration
       t.string :name, null: false
       t.references :user, null: false
       t.references :category, null: false
+      t.integer :popularity, default: 1
+      t.integer :parent_id
 
       t.timestamps null: false
     end
