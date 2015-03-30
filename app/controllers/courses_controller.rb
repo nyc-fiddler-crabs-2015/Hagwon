39f2b0009ec57f8deabafa208875c68661502cc3
Rayanb <rayanbouts@gmail.com>
2015-03-30 18:24:30 -0400

    refactoring

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,26 +1,33 @@
 class CoursesController < ApplicationController
 
-  def index
-    track = Track.includes(:courses => :reviews).find(params[:track_id])
+  def sort_courses(track)
     order_array = Track.find(params[:track_id]).order.split(",").to_a.map{|x| x.to_i}
-    courses = track.courses.find(order_array).index_by(&:id).values_at(*order_array)
-    hey = UserCourse.where(user_id: session[:user_id]).map{|course| course.course_id}
-    user_courses = hey if hey.length > 0
-    render json: {courses: courses.to_json(:include => :reviews), userCourses: user_courses || [-1] }
+    return track.courses.includes(:reviews).find(order_array).index_by(&:id).values_at(*order_array)
   end
 
-  #User has many courses through UserCourses
-  #A course has many users through UserCourses
+
+  def index
+    track = Track.includes(:courses => :reviews).find(params[:track_id])
+    course_ids = UserCourse.where(user_id: session[:user_id]).map{|course| course.course_id}
+    course_ids = [-1] if course_ids.length == 0
+    render json: {courses: sort_courses(track).to_json(:include => :reviews), userCourses: course_ids}
+  end
+
 
   def check
-    UserCourse.create(user_id: session[:user_id], course_id: params[:course_id])
+    info = {user_id: session[:user_id], course_id: params[:course_id]}
+    if UserCourse.find_by(info) == nil
+      UserCourse.create(info)
+    end
     redirect_to :back
   end
 
   def uncheck
-    course = UserCourse.find_by(user_id: session[:user_id], course_id: params[:course_id])
-    course.destroy if course
-    course.save
+    subscription = UserCourse.find_by(user_id: session[:user_id], course_id: params[:course_id])
+    if subscription
+      subscription.destroy
+      subscription.save
+    end
     redirect_to :back
   end
 
