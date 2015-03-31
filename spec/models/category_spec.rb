9d6a2bafb203ae1a52e4c773e5a0652862ff3663
bruce wayne <no1_ldc@hotmail.com>
2015-03-31 18:54:49 -0400

    category model spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,15 +1,4 @@
-# require 'rails_helper'
-
-# describe Category do
-#    it 'is invalid without a category name' do
-#     category = Category.new(name: nil)
-#     category.valid?
-#     expect(category.errors[:name]).not_to include("can't be blank")
-#   end
-# end
-
 require 'rails_helper'
-require 'factory_girl_rails'
 
 describe Category do
 
