43039ae2075e2a99f1e15b138e5ed9bf0663b6a3
Rayan <rayanbouts@gmail.com>
2015-03-30 22:50:19 -0400

    parse from codeschool done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,21 @@
 # User.first.tracks.create(user_id: 1, category_id: 1, name: 'yeee') will assign this track to an owner(User.first), and a user (means that the owner will be able to access it> adding a user_id will store the result in the join table, therefore making it accessible)
 
 require 'open-uri'
-require 'json'
+# require 'json'
 
-result = JSON.parse(open("https://api.coursera.org/api/catalog.v1/categories?includes=courses").read)['elements']
+# result = JSON.parse(open("https://api.coursera.org/api/catalog.v1/categories?includes=courses").read)['elements']
 
-result.each do |category|
-  cat = Category.create(name: category['name'])
-  category['links']['courses'].each do |course_id|
-    begin
-      course = JSON.parse(open("https://api.coursera.org/api/catalog.v1/courses/#{course_id}").read)['elements'][0]
-      Course.create(name: course['name'], url: "https://www.coursera.org/course/#{course['shortName']}", category_id: cat.id)
-    rescue OpenURI::HTTPError => ex
-      next
-    end
-  end
-end
+# result.each do |category|
+#   cat = Category.create(name: category['name'])
+#   category['links']['courses'].each do |course_id|
+#     begin
+#       course = JSON.parse(open("https://api.coursera.org/api/catalog.v1/courses/#{course_id}").read)['elements'][0]
+#       Course.create(name: course['name'], url: "https://www.coursera.org/course/#{course['shortName']}", category_id: cat.id)
+#     rescue OpenURI::HTTPError => ex
+#       next
+#     end
+#   end
+# end
 
 
 
@@ -35,16 +35,16 @@ end
 #yo[0].css('img')[0].attributes['src']
 # h = doc.css('article').map{|x|
 
-require 'nokogiri'
 category = Category.find(7)
+require 'nokogiri'
 doc = Nokogiri::HTML(open("https://www.codeschool.com/courses"))
 
 array_elements = doc.css('article').map{|x| x.children}
-
+# array_elements[1].children[5].children[1].children[0].text
 array_elements.each do |el|
   course = {photo_url: el.css('img')[0].attributes['src'].value,
     url: "http://codeschool.com#{el[1].children[1].attributes['href'].value}",
-    name: el.children[3].children[3].children[1].children.last.text}
+    name: el.children[5].children[1].children[0].text}
   category.courses.create(course)
   puts "SEEDED"
 end
