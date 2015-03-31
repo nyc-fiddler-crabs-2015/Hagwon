6b7a217b578306221a9fc7457cf929a720281c8b
bruce wayne <no1_ldc@hotmail.com>
2015-03-31 17:09:36 -0400

    add categoty_spec using factory_girl

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,9 +1,26 @@
+# require 'rails_helper'
+
+# describe Category do
+#    it 'is invalid without a category name' do
+#     category = Category.new(name: nil)
+#     category.valid?
+#     expect(category.errors[:name]).not_to include("can't be blank")
+#   end
+# end
+
 require 'rails_helper'
+require 'factory_girl_rails'
 
 describe Category do
-   it 'is invalid without a category name' do
-    category = Category.new(name: nil)
-    category.valid?
-    expect(category.errors[:name]).not_to include("can't be blank")
+
+  let(:category) { FactoryGirl.build(:category) }
+
+  it 'has a valid factory' do
+    expect(true).to eq true
   end
+
+  it 'has a name' do
+    expect(category.name).to be_kind_of(String)
+  end
+
 end
