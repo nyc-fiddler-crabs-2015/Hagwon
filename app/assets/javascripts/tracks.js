222e0ce19da69985cf6d7e91e3b3a67a58c3166f
Rayanb <rayanbouts@gmail.com>
2015-04-07 19:20:15 -0400

    delete filter, useless

--- app/assets/javascripts/tracks.js
+++ app/assets/javascripts/tracks.js
@@ -6,11 +6,3 @@ Tracks.run(['$http', function($http) {
   $http.defaults.headers.common['Accept'] = 'application/json';
   $http.defaults.headers.common['Content-Type'] = 'application/json';
 }]);
-
-
-//Add a filter that leaves html safe
-Tracks.filter('unsafe', function($sce) {
-  return function(val) {
-    return $sce.trustAsHtml(val);
-  };
-});
