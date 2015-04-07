b759576dfb1867f77605fd7fa1995d2ecb38106e
Rayanb <rayanbouts@gmail.com>
2015-04-07 00:08:18 -0400

    include users too, to know if course completed

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -7,10 +7,10 @@ class CoursesController < ApplicationController
 
 
   def index
-    track = Track.includes(:courses => :reviews).find(params[:track_id])
+    track = Track.includes(:courses => [:reviews, :platform, :users]).find(params[:track_id])
     course_ids = UserCourse.where(user_id: session[:user_id]).map{|course| course.course_id}
     course_ids = [-1] if course_ids.length == 0
-    render json: {courses: sort_courses(track).to_json(:include => :reviews), userCourses: course_ids}
+    render json: {courses: sort_courses(track).to_json(:include => [:reviews, :platform, :users => {:only => :id}]), userCourses: course_ids}
   end
 
   def check
